c691c81b6d0d81e25702c3f82e6fa42d9043995b
mattsirota <mattsirota@msn.com>
2020-04-01 00:58:46 -0400

    added all the files for the sorter

---
+++ src/js/data/2020-04-01.js
@@ -0,0 +1,299 @@
+dataSetVersion = "2020-04-01"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+  {
+    
+  }
+];
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "Holy Ghost",
+    img: "https://upload.wikimedia.org/wikipedia/en/0/07/Modernbaseballholyghost.jpg",
+    opts: {
+    }
+  },
+  {
+    name: "Wedding Singer",
+    img: "https://upload.wikimedia.org/wikipedia/en/0/07/Modernbaseballholyghost.jpg",
+    opts: {
+    }
+  },
+  {
+    name: "Note To Self",
+    img: "https://upload.wikimedia.org/wikipedia/en/0/07/Modernbaseballholyghost.jpg",
+    opts: {
+    }
+  },
+  {
+    name: "Mass",
+    img: "https://upload.wikimedia.org/wikipedia/en/0/07/Modernbaseballholyghost.jpg",
+    opts: {
+    }
+  },
+  {
+    name: "Everyday",
+    img: "https://upload.wikimedia.org/wikipedia/en/0/07/Modernbaseballholyghost.jpg",
+    opts: {
+    }
+  },
+  {
+    name: "Hiding",
+    img: "https://upload.wikimedia.org/wikipedia/en/0/07/Modernbaseballholyghost.jpg",
+    opts: {
+    }
+  },
+  {
+    name: "Coding These To Lukens",
+    img: "https://upload.wikimedia.org/wikipedia/en/0/07/Modernbaseballholyghost.jpg",
+    opts: {
+    }
+  },
+  {
+    name: "Breathing In Stero",
+    img: "https://upload.wikimedia.org/wikipedia/en/0/07/Modernbaseballholyghost.jpg",
+    opts: {
+    }
+  },
+  {
+    name: "Apple Cider, I Don't Mind",
+    img: "https://upload.wikimedia.org/wikipedia/en/0/07/Modernbaseballholyghost.jpg",
+    opts: {
+    }
+  },
+  {
+    name: "What If...",
+    img: "https://upload.wikimedia.org/wikipedia/en/0/07/Modernbaseballholyghost.jpg",
+    opts: {
+    }
+  },
+  {
+    name: "Just Another Face",
+    img: "https://upload.wikimedia.org/wikipedia/en/0/07/Modernbaseballholyghost.jpg",
+    opts: {
+    }
+  },
+  {
+    name: "Fine, Great",
+    img: "https://upload.wikimedia.org/wikipedia/en/4/4a/Modern_Baseball_You%27re_Gonna_Miss_It_All.png",
+    opts: {
+    }
+  },
+  {
+    name: "Broken Cash Machine",
+    img: "https://upload.wikimedia.org/wikipedia/en/4/4a/Modern_Baseball_You%27re_Gonna_Miss_It_All.png",
+    opts: {
+    }
+  },
+  {
+    name: "Rock Bottom",
+    img: "https://upload.wikimedia.org/wikipedia/en/4/4a/Modern_Baseball_You%27re_Gonna_Miss_It_All.png",
+    opts: {
+    }
+  },
+  {
+    name: "Apartment",
+    img: "https://upload.wikimedia.org/wikipedia/en/4/4a/Modern_Baseball_You%27re_Gonna_Miss_It_All.png",
+    opts: {
+    }
+  },
+  {
+    name: "The Old Gospel Choir",
+    img: "https://upload.wikimedia.org/wikipedia/en/4/4a/Modern_Baseball_You%27re_Gonna_Miss_It_All.png",
+    opts: {
+    }
+  },
+  {
+    name: "Notes",
+    img: "https://upload.wikimedia.org/wikipedia/en/4/4a/Modern_Baseball_You%27re_Gonna_Miss_It_All.png",
+    opts: {
+    }
+  },
+  {
+    name: "Charlie Black",
+    img: "https://upload.wikimedia.org/wikipedia/en/4/4a/Modern_Baseball_You%27re_Gonna_Miss_It_All.png",
+    opts: {
+    }
+  },
+  {
+    name: "Timmy Bowers",
+    img: "https://upload.wikimedia.org/wikipedia/en/4/4a/Modern_Baseball_You%27re_Gonna_Miss_It_All.png",
+    opts: {
+    }
+  },
+  {
+    name: "Going To Bed Now",
+    img: "https://upload.wikimedia.org/wikipedia/en/4/4a/Modern_Baseball_You%27re_Gonna_Miss_It_All.png",
+    opts: {
+    }
+  },
+  {
+    name: "Your Graduation",
+    img: "https://upload.wikimedia.org/wikipedia/en/4/4a/Modern_Baseball_You%27re_Gonna_Miss_It_All.png",
+    opts: {
+    }
+  },
+  {
+    name: "Two Good Things",
+    img: "https://upload.wikimedia.org/wikipedia/en/4/4a/Modern_Baseball_You%27re_Gonna_Miss_It_All.png",
+    opts: {
+    }
+  },
+  {
+    name: "Pothole",
+    img: "https://upload.wikimedia.org/wikipedia/en/4/4a/Modern_Baseball_You%27re_Gonna_Miss_It_All.png",
+    opts: {
+    }
+  },
+  {
+    name: "Re-Do",
+    img: "https://upload.wikimedia.org/wikipedia/en/9/9c/Sports_Modern_Baseball_album.jpg",
+    opts: {
+    }
+  },
+  {
+    name: "Tears Over Beers",
+    img: "https://upload.wikimedia.org/wikipedia/en/9/9c/Sports_Modern_Baseball_album.jpg",
+    opts: {
+    }
+  },
+  {
+    name: "The Weekends",
+    img: "https://upload.wikimedia.org/wikipedia/en/9/9c/Sports_Modern_Baseball_album.jpg",
+    opts: {
+    }
+  },
+  {
+    name: "@chl03k",
+    img: "https://upload.wikimedia.org/wikipedia/en/9/9c/Sports_Modern_Baseball_album.jpg",
+    opts: {
+    }
+  },
+  {
+    name: "Hours Outside in the Snow",
+    img: "https://upload.wikimedia.org/wikipedia/en/9/9c/Sports_Modern_Baseball_album.jpg",
+    opts: {
+    }
+  },
+  {
+    name: "I Think You Were In My Profile Picture Once",
+    img: "https://upload.wikimedia.org/wikipedia/en/9/9c/Sports_Modern_Baseball_album.jpg",
+    opts: {
+    }
+  },
+  {
+    name: "Re-Done",
+    img: "https://upload.wikimedia.org/wikipedia/en/9/9c/Sports_Modern_Baseball_album.jpg",
+    opts: {
+    }
+  },
+  {
+    name: "Cooke",
+    img: "https://upload.wikimedia.org/wikipedia/en/9/9c/Sports_Modern_Baseball_album.jpg",
+    opts: {
+    }
+  },
+  {
+    name: "See Ya, Sucker",
+    img: "https://upload.wikimedia.org/wikipedia/en/9/9c/Sports_Modern_Baseball_album.jpg",
+    opts: {
+    }
+  },
+  {
+    name: "Look Out",
+    img: "https://upload.wikimedia.org/wikipedia/en/9/9c/Sports_Modern_Baseball_album.jpg",
+    opts: {
+    }
+  },
+  {
+    name: "Play Ball!",
+    img: "https://upload.wikimedia.org/wikipedia/en/9/9c/Sports_Modern_Baseball_album.jpg",
+    opts: {
+    }
+  },
+  {
+    name: "Coals",
+    img: "https://upload.wikimedia.org/wikipedia/en/9/9c/Sports_Modern_Baseball_album.jpg",
+    opts: {
+    }
+  },
+  {
+    name: "The Waterboy Returns",
+    img: "https://images-na.ssl-images-amazon.com/images/I/71eeJrbZuqL._SY355_.jpg",
+    opts: {
+    }
+  },
+  {
+    name: "Alpha Kappa Fall Of Troy The Movie Part Deux",
+    img: "https://images-na.ssl-images-amazon.com/images/I/71eeJrbZuqL._SY355_.jpg",
+    opts: {
+    }
+  },
+  {
+    name: "Infinity Exposed",
+    img: "https://images-na.ssl-images-amazon.com/images/I/71eeJrbZuqL._SY355_.jpg",
+    opts: {
+    }
+  },
+  {
+    name: "The Thrash Particle",
+    img: "https://images-na.ssl-images-amazon.com/images/I/71eeJrbZuqL._SY355_.jpg",
+    opts: {
+    }
+  },
+  {
+    name: "...And Beyond",
+    img: "https://images-na.ssl-images-amazon.com/images/I/71eeJrbZuqL._SY355_.jpg",
+    opts: {
+    }
+  },
+  {
+    name: "Revenge of the Nameless Ranger",
+    img: "https://images-na.ssl-images-amazon.com/images/I/71eeJrbZuqL._SY355_.jpg",
+    opts: {
+    }
+  },
+  {
+    name: "Hope",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81dDJeY7UgL._SY355_.jpg",
+    opts: {
+    }
+  },
+  {
+    name: "It's Cold Out Here",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81dDJeY7UgL._SY355_.jpg",
+    opts: {
+    }
+  },
+  {
+    name: "Best Friend",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81hwMQ6GkGL._SL1500_.jpg",
+    opts: {
+    }
+  },
+  {
+    name: "Short",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81hwMQ6GkGL._SL1500_.jpg",
+    opts: {
+    }
+  },
+  {
+    name: "My Love",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81hwMQ6GkGL._SL1500_.jpg",
+    opts: {
+    }
+  },
+  {
+    name: "Casket",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81hwMQ6GkGL._SL1500_.jpg",
+    opts: {
+    }
+  },
+  {
+    name: "Home",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81hwMQ6GkGL._SL1500_.jpg",
+    opts: {
+    }
+  }
+];
